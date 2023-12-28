97393e953b6d9d6dd5528ba74f71b462e511af87
xiaoxi-ij478 <3432611803@qq.com>
2023-12-29 00:46:56 +0800

    将项目转到正经仓库
    项目原本放在 Gist 上, 现在搬到正经仓库里面了.
    加了 ISO 解密脚本和 README.

--- file_decrypt.py
+++ file_decrypt.py
@@ -8,6 +8,7 @@ import zlib
 from abc import ABCMeta, abstractmethod
 from typing import ByteString
 
+
 class DataDecryptor(metaclass=ABCMeta):
     @abstractmethod
     def encrypt(self, bytestream: ByteString) -> bytes:
@@ -67,6 +68,7 @@ class DT01DataDecrypter(DataDecryptor):
 
         return zlib.decompress(bytestream)
 
+
 class DT03DataDecrypter(DataDecryptor):
     _LEN: int = 77
 
@@ -91,8 +93,10 @@ class DT03DataDecrypter(DataDecryptor):
     def encrypt(self, bytestream: ByteString) -> bytes:
         return self._internal(bytestream, len(bytestream))
 
+
 class UnmatchMagicNumberError(ValueError):
     pass
+
 
 class EncryptManager:
     encryptors: dict[int, DataDecryptor] = {
@@ -101,6 +105,8 @@ class EncryptManager:
     }
 
     def encrypt(self, enctype: int, bytestream: ByteString) -> bytes:
+        print("使用加密算法", enctype, file=sys.stderr)
+
         encryptor = self.encryptors[enctype]
 
         bytestream = encryptor().encrypt(bytestream)
@@ -113,48 +119,51 @@ class EncryptManager:
         if bytestream[0:3] != b"\x3F\x3F\x00":
             raise UnmatchMagicNumberError("Unmatch magic number")
 
-        print("Using encryption algorithm", bytestream[3], file=sys.stderr)
+        print("使用加密算法", bytestream[3], file=sys.stderr)
 
         decryptor = self.encryptors[bytestream[3]]
 
         return decryptor().decrypt(bytestream[4:])
 
+
 def print_help(prog_name):
-    print(f"Usage: {prog_name} [OPTIONS]... [FILENAME]", file=sys.stderr)
-    print("Encrypt/Decrypt", file=sys.stderr)
+    print(f"使用方法: {prog_name} [选项]... [输入文件]", file=sys.stderr)
+    print("加密 / 解密人教社光盘的文件 v0.0.1", file=sys.stderr)
     print(file=sys.stderr)
-    print("File may be '-' to read from stdin / write to stdout", file=sys.stderr)
-    print("Default is decrypt stdin to stdout", file=sys.stderr)
+    print("输入 / 输出文件可以是 `-' 以从标准输入读取 / 写入到标准输出", file=sys.stderr)
+    print("默认行为是解密标准输入到标准输出", file=sys.stderr)
     print(file=sys.stderr)
-    print("  -e, --encrypt <ALGORITHM>  Encrypt file (1 for algo 1, 3 for algo 3)", file=sys.stderr)
-    print("  -d, --decrypt              Decrypt file (default)", file=sys.stderr)
-    print("  -o, --output <FILENAME>    Write output to <FILENAME> (default stdout)", file=sys.stderr)
-    print("                             (throw an error if <FILENAME> exists", file=sys.stderr)
-    print("                              and `-r' is not specified)", file=sys.stderr)
-    print("  -f, --dir <DIRECTORY>      Put the output file at <DIRECTORY>", file=sys.stderr)
-    print("                             (cannot use with `-r')", file=sys.stderr)
-    print("  -r, --replace              Without `-o', replace the input file", file=sys.stderr)
-    print("                             With `-o', replace the specified output file", file=sys.stderr)
-    print("  -h, --help                 Display this help", file=sys.stderr)
+    print("  -e, --encrypt <算法 ID>   使用 <算法 ID> 加密文件", file=sys.stderr)
+    print("  -d, --decrypt             解密文件 (默认行为)", file=sys.stderr)
+    print("  -o, --output <输出文件>   写入输出到 <输出文件> (默认为标准输出)", file=sys.stderr)
+    print("                            (如果 <输出文件> 存在且 `-r' 没有指定, 则抛出错误)", file=sys.stderr)
+    # -d, -r 都被用了 :O
+    print("  -f, --dir <目录>          将输出文件放置在 <目录> 下", file=sys.stderr)
+    print("                            (不可与 `-r' 同时使用)", file=sys.stderr)
+    print("  -r, --replace             如果没有 `-o', 则替换输入文件 (除非是标准输入)", file=sys.stderr)
+    print("                            如果有 `-o', 则强制覆盖指定的输出文件", file=sys.stderr)
+    print("  -h, --help                显示这个帮助", file=sys.stderr)
 
 def main(argc, argv):
+    # 我的命令行写的就是个垃圾, 凑合看吧
+
     command_line = getopt.gnu_getopt(
         argv[1:], "e:do:rf:h",
         ["encrypt=", "decrypt", "output=", "replace", "dir=", "help"]
     )
     input_file = sys.stdin.buffer
     output_file = '-'
-    exec_function = EncryptManager().decrypt
     replace = False
+    encrypt = False
     output_dir = pathlib.Path()
 
     for option, argument in command_line[0]:
         if option in ("-e", "--encrypt"):
-            encrypt_algorithm = argument
-            exec_function = lambda b: EncryptManager().encrypt(int(encrypt_algorithm), b)
+            encrypt_algorithm = int(argument)
+            encrypt = True
 
         elif option in ("-d", "--decrypt"):
-            exec_function = EncryptManager().decrypt
+            encrypt = False
 
         elif option in ("-o", "--output"):
             output_file = argument
@@ -171,20 +180,44 @@ def main(argc, argv):
 
     if command_line[1]:
         input_file = sys.stdin.buffer if command_line[1] == '-' else open(command_line[1][0], "rb")
+
+        # 如果指定了输入文件和替换文件, 那么把输出文件设置为输入文件
         if output_file == '-' and replace:
             output_file = command_line[1][0]
 
     data = input_file.read()
 
-    try:
-        processed_data = exec_function(data)
-    except UnmatchMagicNumberError:
-        print("File is not encrypted", file=sys.stderr)
-        return 1
+    if encrypt:
+        # 如果有 <文件名>.enc_algo, 从那里获取加密算法 ID
+        # 前提是不从标准输入读数据
+
+        # 当前版本就算有 .enc_algo, 命令行也得指定一个加密算法 ID
+        # 因为 Python 的 getopt 库还不支持 e:: 这种选项
+        # (我不想学什么 optparse)
+        if (
+            input_file is not sys.stdin.buffer and
+            os.access(input_file.name + ".enc_algo", os.R_OK)
+        ):
+            encrypt_algorithm = int(open(input_file.name + ".enc_algo").read())
+
+        processed_data = EncryptManager().encrypt(encrypt_algorithm, data)
+    else:
+        try:
+            processed_data = EncryptManager().decrypt(data)
+        except UnmatchMagicNumberError:
+            print("文件未加密", file=sys.stderr)
+            return 1
 
     if output_file == '-':
         sys.stdout.buffer.write(processed_data)
     else:
+        # 把加密算法 ID 写到<文件名>.enc_algo 里,
+        # 下次加密的时候用 (如果写不进去就算了)
+        try:
+            print(data[3], file=open(output_dir / (output_file + ".enc_algo"), "w"))
+        except:
+            pass
+
         with open(output_dir / output_file, "wb" if replace else "xb") as output_file_obj:
             output_file_obj.write(processed_data)
 
---
+++ iso_decrypt.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import getopt
+import io
+import pathlib
+import sys
+
+# 基本从 file_decrypt.py 里照搬了
+def print_help(prog_name):
+    print(f"使用方法: {prog_name} [选项]... [输入文件]", file=sys.stderr)
+    print("解密人教社光盘 ISO v0.0.1", file=sys.stderr)
+    print(file=sys.stderr)
+    print("输入 / 输出文件可以是 `-' 以从标准输入读取 / 写入到标准输出", file=sys.stderr)
+    print("默认行为是解密标准输入到标准输出", file=sys.stderr)
+    print(file=sys.stderr)
+    print("  -o, --output <输出文件>   写入输出到 <输出文件> (默认为标准输出)", file=sys.stderr)
+    print("                            (如果 <输出文件> 存在且 `-r' 没有指定, 则抛出错误)", file=sys.stderr)
+    # -d, -r 都被用了 :O
+    print("  -f, --dir <目录>          将输出文件放置在 <目录> 下", file=sys.stderr)
+    print("                            (不可与 `-r' 同时使用)", file=sys.stderr)
+    print("  -r, --replace             如果没有 `-o', 则替换输入文件 (除非是标准输入)", file=sys.stderr)
+    print("                            如果有 `-o', 则强制覆盖指定的输出文件", file=sys.stderr)
+    print("  -h, --help                显示这个帮助", file=sys.stderr)
+
+def main(argc, argv):
+    command_line = getopt.gnu_getopt(
+        argv[1:], "e:do:rf:h",
+        ["encrypt=", "decrypt", "output=", "replace", "dir=", "help"]
+    )
+    input_file = sys.stdin.buffer
+    output_file = '-'
+    replace = False
+    output_dir = pathlib.Path()
+
+    for option, argument in command_line[0]:
+        if option in ("-o", "--output"):
+            output_file = argument
+
+        elif option in ("-r", "--replace"):
+            replace = True
+
+        elif option in ("-f", "--dir"):
+            output_dir = pathlib.Path(argument)
+
+        elif option in ("-h", "--help"):
+            print_help(argv[0])
+            return 0
+
+    if command_line[1]:
+        input_file = sys.stdin.buffer if command_line[1] == '-' else open(command_line[1][0], "rb")
+
+        # 如果指定了输入文件和替换文件, 那么把输出文件设置为输入文件
+        if output_file == '-' and replace:
+            output_file = command_line[1][0]
+
+    # ISO 的结构: https://wiki.osdev.org/ISO_9660
+    # data 原本是文件夹, 但在 ISO 里用 flag 被标记成了一个文件
+    # 我们把 flag 改成文件夹即可
+
+    # 输入文件可能是标准输入, 我们没法确定它的大小
+    # 所以用传统的 .read()
+    data = io.BytesIO(input_file.read())
+
+    if len(data.getbuffer()) < 0x8000:
+        # 文件被截断 (有可能直接在终端里读标准输入了)
+        print("文件被截断", file=sys.stderr)
+        return 1
+
+    # LBA = ?
+    data.seek(0x8882)
+    lba_size = int.from_bytes(data.read(2))
+
+    # goto root directory entry
+    data.seek(0x88A2)
+    data.seek(lba_size * int.from_bytes(data.read(4)))
+
+    # 找 "data/"
+    while True:
+        length = int.from_bytes(data.read(2), "little")
+        tmp_data = data.read(length - 2)
+        if b"d\x00a\x00t\x00a\x00" in tmp_data:
+            data.seek(-length, io.SEEK_CUR)
+            break
+
+    # 修改 flag
+    data.seek(25, io.SEEK_CUR)
+    data.write(b'\x06')
+
+    if output_file == '-':
+        sys.stdout.buffer.write(data.getbuffer())
+    else:
+        with open(output_dir / output_file, "wb" if replace else "xb") as output_file_obj:
+            output_file_obj.write(data.getbuffer())
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(len(sys.argv), sys.argv))
